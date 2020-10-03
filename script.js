8a174f2a950858475724fb77559d974764d9c33b
GiacomoGitHub <poddagiacomo@gmail.com>
2020-10-03 11:31:01 +0200

    Display works and buttons change on click

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
-// OPERATIONS
-function add(a, b) {
-    return a + b;
+
+
+// STORE WHAT HAS TO APPEAR IN DISPLAY
+// let displayValue = "";
+
+
+// STORE NUMBER
+let number = "";
+
+
+// STORE THE OPERATOR
+let operator = "";
+
+
+// LISTEN TO NUMBER CLICKS AND UPDATE DISPLAY
+document.getElementById("one").addEventListener("click", function () { number += 1; updateDisplay()});
+document.getElementById("two").addEventListener("click", function () { number += 2; updateDisplay()});
+document.getElementById("three").addEventListener("click", function () { number += 3; updateDisplay()});
+document.getElementById("four").addEventListener("click", function () { number += 4; updateDisplay()});
+document.getElementById("five").addEventListener("click", function () { number += 5; updateDisplay()});
+document.getElementById("six").addEventListener("click", function () { number += 6; updateDisplay()});
+document.getElementById("seven").addEventListener("click", function () { number += 7; updateDisplay()});
+document.getElementById("eight").addEventListener("click", function () { number += 8; updateDisplay()});
+document.getElementById("nine").addEventListener("click", function () { number += 9; updateDisplay()});
+document.getElementById("zero").addEventListener("click", function () { number += 0; updateDisplay()});
+
+
+// LISTEN TO OPERATOR CLICKS AND STORE IT IN VAR OPERATOR
+document.getElementById("percentage").addEventListener("click", function () { operator = "%";  });
+document.getElementById("divide").addEventListener("click", function () { operator = "/" });
+document.getElementById("multiply").addEventListener("click", function () { operator = "x" });
+document.getElementById("subtract").addEventListener("click", function () { operator = "-" });
+document.getElementById("add").addEventListener("click", function () { operator = "+" });
+
+
+// CLEAR DISPLAY
+document.getElementById("clear").addEventListener("click", function () { number = ""; updateDisplay()});
+
+
+// SHOW DATA IN DISPLAY
+function updateDisplay () {
+    document.getElementById("display").textContent = number;
 }
 
-function subtract(a, b) {
-    return a - b;
-}
 
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
+// OPERATE AND RETURN THE RESULT WHEN CLICK ON "="
+document.getElementById("equal").addEventListener("click", operate());
 
 
 // CHECK THE OPERATION THAT NEEDS TO BE RUN
-function operate(operator, a, b) {
+function operate(operator) {
     if (operator === '+') {
         add();
     } else if (operator === '-') {
@@ -30,29 +61,19 @@ function operate(operator, a, b) {
 }
 
 
-// STORE WHAT HAS TO APPEAR IN DISPLAY
-let displayValue = "";
+// OPERATE AND SHOW RESULT
+function add(number, b) {
+    document.getElementById("display").textContent = number += b;
+}
 
-// LISTEN TO NUMBER CLICKS
-document.getElementById("one").addEventListener("click", function () { displayValue += 1});
-document.getElementById("two").addEventListener("click", function () { displayValue += 2});
-document.getElementById("three").addEventListener("click", function () { displayValue += 3});
-document.getElementById("four").addEventListener("click", function () { displayValue += 4});
-document.getElementById("five").addEventListener("click", function () { displayValue += 5});
-document.getElementById("six").addEventListener("click", function () { displayValue += 6});
-document.getElementById("seven").addEventListener("click", function () { displayValue += 7});
-document.getElementById("eight").addEventListener("click", function () { displayValue += 8});
-document.getElementById("nine").addEventListener("click", function () { displayValue += 9});
-document.getElementById("zero").addEventListener("click", function () { displayValue += 0});
+function subtract(number, b) {
+    document.getElementById("display").textContent = number -= b;
+}
 
+function multiply(number, b) {
+    document.getElementById("display").textContent = number *= b;
+}
 
-// LISTEN TO OPERATOR CLICKS
-document.getElementById("percentage").addEventListener("click", function () { displayValue += " % "});
-document.getElementById("divide").addEventListener("click", function () { displayValue += " / "});
-document.getElementById("multiply").addEventListener("click", function () { displayValue += " x "});
-document.getElementById("subtract").addEventListener("click", function () { displayValue += " - "});
-document.getElementById("add").addEventListener("click", function () { displayValue += " + "});
-
-
-// CLEAR DISPLAY
-document.getElementById("clear").addEventListener("click", function () { displayValue = ""});
+function divide(number, b) {
+    document.getElementById("display").textContent = number /= b;
+}
