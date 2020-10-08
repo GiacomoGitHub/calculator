49539cca08c1280385646290c89b25849902d7c5
GiacomoGitHub <poddagiacomo@gmail.com>
2020-10-08 20:26:44 +0200

    Fixed division per zero and percentage bug

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // STORE INPUT NUMBER
-let inputNumber = ("");
+let inputNumber = "";
 
 
 
 /// STORE STORED NUMBER
-let storedNumber = Number("");
+let storedNumber = "";
 
 
 
@@ -14,41 +14,45 @@ let operator = "";
 
 
 // STORE RESULT
-let result = Number("");
+let result = 0;
 
 
 // LISTEN TO NUMBER CLICKS AND UPDATE DISPLAY
-document.getElementById("one").addEventListener("click", function () { inputNumber += 1; updateDisplay()});
-document.getElementById("two").addEventListener("click", function () { inputNumber += 2; updateDisplay()});
-document.getElementById("three").addEventListener("click", function () { inputNumber += 3; updateDisplay()});
-document.getElementById("four").addEventListener("click", function () { inputNumber += 4; updateDisplay()});
-document.getElementById("five").addEventListener("click", function () { inputNumber += 5; updateDisplay()});
-document.getElementById("six").addEventListener("click", function () { inputNumber += 6; updateDisplay()});
-document.getElementById("seven").addEventListener("click", function () { inputNumber += 7; updateDisplay()});
-document.getElementById("eight").addEventListener("click", function () { inputNumber += 8; updateDisplay()});
-document.getElementById("nine").addEventListener("click", function () { inputNumber += 9; updateDisplay()});
-document.getElementById("zero").addEventListener("click", function () { inputNumber += 0; updateDisplay()});
-document.getElementById("comma").addEventListener("click", function () { inputNumber += "."; updateDisplay()});
+document.getElementById("one").addEventListener("click", function () { inputNumber += 1; updateDisplay() });
+document.getElementById("two").addEventListener("click", function () { inputNumber += 2; updateDisplay() });
+document.getElementById("three").addEventListener("click", function () { inputNumber += 3; updateDisplay() });
+document.getElementById("four").addEventListener("click", function () { inputNumber += 4; updateDisplay() });
+document.getElementById("five").addEventListener("click", function () { inputNumber += 5; updateDisplay() });
+document.getElementById("six").addEventListener("click", function () { inputNumber += 6; updateDisplay() });
+document.getElementById("seven").addEventListener("click", function () { inputNumber += 7; updateDisplay() });
+document.getElementById("eight").addEventListener("click", function () { inputNumber += 8; updateDisplay() });
+document.getElementById("nine").addEventListener("click", function () { inputNumber += 9; updateDisplay() });
+document.getElementById("zero").addEventListener("click", function () { if (inputNumber === "") { inputNumber = 0; } else { inputNumber += "0"; updateDisplay() } });
+document.getElementById("comma").addEventListener("click", function () { inputNumber += "."; updateDisplay() });
 
 
 
 // LISTEN TO OPERATOR CLICKS, STORE ITS VALUE, STORES PREVIOUS NUMBER AND RESETS INPUT NUMBER
-document.getElementById("percentage").addEventListener("click", function () { operator = "%"; storedNumber = inputNumber; inputNumber = 0; });
-document.getElementById("divide").addEventListener("click", function () { operator = "/"; storedNumber = inputNumber; inputNumber = 0; });
-document.getElementById("multiply").addEventListener("click", function () { operator = "x"; storedNumber = inputNumber; inputNumber = 0; });
-document.getElementById("subtract").addEventListener("click", function () { operator = "-"; storedNumber = inputNumber; inputNumber = 0; });
-document.getElementById("add").addEventListener("click", function () { operator = "+"; storedNumber = inputNumber; inputNumber = 0; });
+document.getElementById("percentage").addEventListener("click", function () { operator = "%"; storedNumber = inputNumber; inputNumber = ""; });
+document.getElementById("divide").addEventListener("click", function () { operator = "/"; storedNumber = inputNumber; inputNumber = ""; });
+document.getElementById("multiply").addEventListener("click", function () { operator = "x"; storedNumber = inputNumber; inputNumber = ""; });
+document.getElementById("subtract").addEventListener("click", function () { operator = "-"; storedNumber = inputNumber; inputNumber = ""; });
+document.getElementById("add").addEventListener("click", function () { operator = "+"; storedNumber = inputNumber; inputNumber = ""; });
 
 
 
 // CLEAR NUMBER AND OPERATOR
-document.getElementById("clear").addEventListener("click", function () { inputNumber = ""; storedNumber = Number(""); operator = ""; updateDisplay()});
+document.getElementById("clear").addEventListener("click", function () { inputNumber = ""; storedNumber = ""; operator = ""; updateDisplay()});
 
 
 
 // SHOW DATA IN DISPLAY
 function updateDisplay () {
-    document.getElementById("display").textContent = inputNumber;
+    if (inputNumber != "") {
+        document.getElementById("display").textContent = inputNumber;
+    } else {
+        document.getElementById("display").textContent = 0;
+    }
 }
 
 
@@ -84,7 +88,6 @@ function add() {
     result = Number(storedNumber) + Number(inputNumber);
     document.getElementById("display").textContent = result;
     operator = "";
-    inputNumber = "";
     inputNumber = result;
 }
 
@@ -92,7 +95,6 @@ function subtract() {
     result = storedNumber -= inputNumber;
     document.getElementById("display").textContent = result;
     operator = "";
-    inputNumber = "";
     inputNumber = result;
 }
 
@@ -100,7 +102,6 @@ function multiply() {
     result = storedNumber *= inputNumber
     document.getElementById("display").textContent = result;
     operator = "";
-    inputNumber = "";
     inputNumber = result;
 }
 
@@ -108,7 +109,6 @@ function divide() {
     result = storedNumber /= inputNumber
     document.getElementById("display").textContent = result;
     operator = "";
-    inputNumber = "";
     inputNumber = result;
 }
 
@@ -116,6 +116,16 @@ function percentage() {
     result = (inputNumber / 100) * storedNumber;
     document.getElementById("display").textContent = result;
     operator = "";
-    inputNumber = "";
     inputNumber = result;
 }
+
+
+// BUGS
+// addition
+// several operations
+// several commas
+
+// SOLVED 
+// 10% of 100
+// division by 0
+
