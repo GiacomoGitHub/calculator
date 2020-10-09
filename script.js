e214cb94e432faee67b8eee018f282b53f26fd94
GiacomoGitHub <poddagiacomo@gmail.com>
2020-10-09 16:00:56 +0200

    Bug fixes

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ let result = 0;
 
 
 // LISTEN TO NUMBER CLICKS AND UPDATE DISPLAY
-document.getElementById("one").addEventListener("click", function () { inputNumber += 1; updateDisplay() });
-document.getElementById("two").addEventListener("click", function () { inputNumber += 2; updateDisplay() });
-document.getElementById("three").addEventListener("click", function () { inputNumber += 3; updateDisplay() });
-document.getElementById("four").addEventListener("click", function () { inputNumber += 4; updateDisplay() });
-document.getElementById("five").addEventListener("click", function () { inputNumber += 5; updateDisplay() });
-document.getElementById("six").addEventListener("click", function () { inputNumber += 6; updateDisplay() });
-document.getElementById("seven").addEventListener("click", function () { inputNumber += 7; updateDisplay() });
-document.getElementById("eight").addEventListener("click", function () { inputNumber += 8; updateDisplay() });
-document.getElementById("nine").addEventListener("click", function () { inputNumber += 9; updateDisplay() });
-document.getElementById("zero").addEventListener("click", function () { if (inputNumber === "") { inputNumber = 0; } else { inputNumber += "0"; updateDisplay() } });
-document.getElementById("comma").addEventListener("click", function () { inputNumber += "."; updateDisplay() });
+document.getElementById("one").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 1; updateDisplay() } });
+document.getElementById("two").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 2; updateDisplay() } });
+document.getElementById("three").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 3; updateDisplay() } });
+document.getElementById("four").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 4; updateDisplay() } });
+document.getElementById("five").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 5; updateDisplay() } });
+document.getElementById("six").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 6; updateDisplay() } });
+document.getElementById("seven").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 7; updateDisplay() } });
+document.getElementById("eight").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 8; updateDisplay() } });
+document.getElementById("nine").addEventListener("click", function () { if (inputNumber.length < 7) { inputNumber += 9; updateDisplay() } });
+document.getElementById("zero").addEventListener("click", function () { if (inputNumber === "") { inputNumber = 0; } else { if (inputNumber.length < 7) inputNumber += "0"; updateDisplay() } });
+document.getElementById("comma").addEventListener("click", function () { if (inputNumber.indexOf('.') === -1) { inputNumber += "."; updateDisplay() } });
 
 
 
@@ -64,7 +64,11 @@ document.getElementById("equal").addEventListener("click", function () { operate
 
 // CHECK THE OPERATION THAT NEEDS TO BE RUN
 function operate() {
-    if (operator === '+') {
+    if (operator === "") {
+        inputNumber = ""; 
+        storedNumber = "";
+        document.getElementById("display").textContent = 0;
+    } else if (operator === '+') {
         add();
     } else if (operator === '-') {
         subtract();
@@ -73,6 +77,10 @@ function operate() {
     } else if (operator === '/') {
         if (inputNumber === 0) {
             document.getElementById("display").textContent = "ERROR";
+            inputNumber = "";
+            storedNumber = "";
+            operator = "";
+            result = 0;
         } else {
             divide();
         }
@@ -86,35 +94,67 @@ function operate() {
 // OPERATE, SHOW RESULT IN DISPLAY, RESET EVERYTHING, MAKE RESULT THE INPUT NUMBER FOR EVENTUAL ADDITIONAL OPERATIONS
 function add() {
     result = Number(storedNumber) + Number(inputNumber);
-    document.getElementById("display").textContent = result;
-    operator = "";
-    inputNumber = result;
+    if (result.toString().length > 10) {
+        document.getElementById("display").textContent = "ERROR";
+        inputNumber = "";
+        storedNumber = "";
+        operator = "";
+        result = 0;
+    } else {
+        document.getElementById("display").textContent = result;
+        operator = "";
+        inputNumber = result;
+    }
 }
 
 function subtract() {
-    result = storedNumber -= inputNumber;
-    document.getElementById("display").textContent = result;
-    operator = "";
-    inputNumber = result;
+    result = (storedNumber -= inputNumber);
+    if (result.toString().length > 10) {
+        document.getElementById("display").textContent = "ERROR";
+        inputNumber = "";
+        storedNumber = "";
+        operator = "";
+        result = 0;
+    } else {
+        document.getElementById("display").textContent = result;
+        operator = "";
+        inputNumber = result;
+    }
 }
 
 function multiply() {
-    result = storedNumber *= inputNumber
-    document.getElementById("display").textContent = result;
-    operator = "";
-    inputNumber = result;
+    result = (storedNumber *= inputNumber);
+    if (result.toString().length > 10) {
+        document.getElementById("display").textContent = "ERROR";
+        inputNumber = "";
+        storedNumber = "";
+        operator = "";
+        result = 0;
+    } else {
+        document.getElementById("display").textContent = result;
+        operator = "";
+        inputNumber = result;
+    }
 }
 
 function divide() {
-    result = storedNumber /= inputNumber
-    document.getElementById("display").textContent = result;
-    operator = "";
-    inputNumber = result;
+    result = (storedNumber /= inputNumber);
+    if (result.toString().length > 10) {
+        document.getElementById("display").textContent = "ERROR";
+        inputNumber = "";
+        storedNumber = "";
+        operator = "";
+        result = 0;
+    } else {
+        document.getElementById("display").textContent = result;
+        operator = "";
+        inputNumber = result;
+    }
 }
 
 function percentage() {
-    result = (inputNumber / 100) * storedNumber;
-    document.getElementById("display").textContent = result;
+    result = Number((inputNumber / 100) * storedNumber);
+    document.getElementById("display").textContent = Number(result.toString().substring(0, 3));
     operator = "";
     inputNumber = result;
 }
@@ -123,9 +163,12 @@ function percentage() {
 // BUGS
 // addition
 // several operations
-// several commas
+
 
 // SOLVED 
 // 10% of 100
 // division by 0
+// rounding length of input numbers
+// several commas
+// rounding result length
 
